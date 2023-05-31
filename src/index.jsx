e9a2597227759d68f65728596531f47dd0cf34a4
Brendon Choi <choi.brendon2001@gmail.com>
2023-06-01 06:04:51 +0900

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import { createRoot } from 'react-dom/client';
+import { MainView } from './components/main-view/main-view';
+import Container from "react-bootstrap/Container";
+
+
+// Import statement to indicate that you need to bundle `./index.scss`
+import "./index.scss";
+
+// Main component (will eventually use all the others)
+
+
+const App = () => {
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
+};
+  
+  const container = document.querySelector('#root');
+  const root = createRoot(container);
+  root.render(<App />);
